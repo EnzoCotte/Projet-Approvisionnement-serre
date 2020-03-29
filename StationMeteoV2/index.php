25cf9b529ce9d767f93523f4a127250926a47913
EnzoCotte <55100678+EnzoCotte@users.noreply.github.com>
2020-03-29 11:26:10 +0200

    Add files via upload
    site

---
+++ StationMeteoV2/index.php
@@ -0,0 +1,30 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css" />
+        <link rel="icon" type="jpg" href="Lowranceonglet.jpg" />
+        <title>Station Meteo</title>
+        <?php 
+            include 'fonction.php'; 
+            Menu()
+        ?>
+    </head>
+
+    <body>
+        <div class="cadre">
+            <div class="titre">
+                <p>Serre Autonome</p>
+            </div>
+            <hr>
+            <div class="sousTitre">
+                <p>Projet de Serre de BTS SN2</p><br>
+                <p>Dans le cadre du projet d’établissement Pro Green Party  a la Providence</p>
+                <p> le but de notre projet est d'automatiser une serre afin de pouvoir l’entretenir</p>
+                <p> même durant les grandes vacances lorsque le personnel n’est pas présent au lycée</p>   
+            </div><br>   
+            <hr>
+            <center><img src="logo.png" alt="logo" /></center>
+        </div>
+    </body>
+</html>
---
+++ StationMeteoV2/parametre.php
@@ -0,0 +1,26 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css" />
+        <link rel="icon" type="jpg" href="Lowranceonglet.jpg" />
+        <title>Station Meteo</title>
+        <?php 
+            include 'fonction.php'; 
+            Menu()
+        ?>
+    </head>
+
+    <body>
+        <div class="cadre">
+            <div class="titre">
+                <p>Serre Autonome</p>
+            </div>
+            <hr>
+            <div class="sousTitre">
+                 
+            </div><br>   
+            
+        </div>
+    </body>
+</html>
