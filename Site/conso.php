462771be3ced3b8ef5cb7deda80e7dd170a235e2
fontainet <54977785+fontainet@users.noreply.github.com>
2020-03-30 02:51:43 +0200

    modif nom

---
+++ Site/conso.php
@@ -0,0 +1,40 @@
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css" />
+        <link rel="icon" type="jpg" href="Lowranceonglet.jpg" />
+        <title>Station Meteo</title>
+        <?php 
+            include 'fonction.php'; 
+            Menu()
+        ?>
+    </head>
+
+    <body>
+        <div class="cadre">
+            <div class="titre">
+                <p>Serre Autonome</p>
+            </div>
+            <hr>
+            <div class="sousTitre">
+                <p>Consommation en temps réel :</p>  
+            </div>   
+            <div class="temperature">
+                <p>3.6 L</p>
+            </div>
+            <div class="sousTitre">
+                <p>Sur le reseau  :</p>  
+            </div> 
+            <div class="temperature">
+                <p>1</p>
+
+
+
+
+
+            </div>
+        </div>
+    </body>
+</html>
